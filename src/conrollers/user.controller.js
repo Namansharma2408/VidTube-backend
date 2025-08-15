301f2fb4749d692572939ff56a8c73281762e2d8
Namansharma2408 <sharmanaman4002@gmail.com>
2025-08-15 22:13:17 +0530

    nothing

--- src/conrollers/user.controller.js
+++ src/conrollers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/index.js";
 
 const registerUser = asyncHandler( async (req,res) => {
     res.status(200).json({
-        message:"OK"
+        message:"Naman Sharma, this is OK"
     })
 })
 
